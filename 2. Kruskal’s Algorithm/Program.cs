a4c673e8c1d607338a8a6f2ba90ee8b197bba51d
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-01-31 21:49:08 +0200

    Add project files.

---
+++ 2. Kruskal’s Algorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphsDijkstra_MST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
